2cf5f1efed1838f0287f02fd78b497a0ef025571
Breno <breno_fari@hotmail.com>
2021-02-28 22:52:01 -0300

    Start Menu

---
+++ Assets/_Scripts/PanelPosition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelPosition : MonoBehaviour
+{
+    GameObject panel;
+
+    public void PanelPositioner()
+    {
+        panel = this.gameObject;
+        panel.GetComponent<RectTransform>().anchoredPosition = new Vector2(-100, 0);
+
+    }
+}
+
+
